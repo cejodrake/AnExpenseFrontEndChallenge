a4c8635ba4c13bbbd79941a0aee37ce2e0b3ccf7
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-07-01 20:20:44 -0700

    update authServices

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,2 +1,26 @@
 import http from './httpServices';
 import { apiUrl } from '../config.json'
+
+
+const apiEndPoint = apiUrl + '/auth';
+const tokenKey = "token";
+
+export async function login(email, password) {
+    const { data: jwt } = await http.post(apiEndPoint, { email, password });
+    localStorage.setItem(tokenKey, jwt);
+}
+
+function logout() {
+    localStorage.removeItem(tokenKey);
+}
+
+export function getCurrentUser() {
+    return null;
+}
+
+export default {
+    login,
+    logout,
+    getCurrentUser
+
+}
