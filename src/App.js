36c5731df94667326aae417664da57eda3786dbf
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-07-01 21:24:05 -0700

    create component navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import LoginForm from './components/loginForm';
+
 
 import './App.css';
-import LoginForm from './components/loginForm';
+import 'react-toastify/dist/ReactTostify.css';
+
+
 
 class App extends Component {
 
---
+++ src/components/common/navBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const NavBar = ({ user }) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" >Dayan Movies Services</a>
+            <button className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+
+                </div>
+            </div>
+        </nav>
+    )
+}
