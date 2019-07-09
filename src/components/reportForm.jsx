53ce14d48c69536d4be5b0d35afeec070a6f7519
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-07-08 20:14:53 -0700

    create FormReport for show information about expenses

--- src/components/reportForm.jsx
+++ src/components/reportForm.jsx
@@ -1,7 +1,23 @@
 import { React } from "react";
 import Form from './common/form';
 
+import Joi from 'joi-browser';
+
+
 class ReactForm extends Form {
+    state = {
+        data: { dateInitial: "", dateEnd: "" },
+        errors: {}
+    }
+
+    schema = {
+        dateInitial: Joi.date().format('YYYY/-MM-DD', 'DD-MM-YYY'),
+        dateEnd: Joi.date().format('YYYY/-MM-DD', 'DD-MM-YYY')
+
+    }
+    doSubmit = async () => {
+
+    }
 
 
 }
