5ca80eab4201ca73fd695c58d8622e9e4865d6a3
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-07-08 20:24:22 -0700

    create FormReport for show information about expenses

--- src/components/reportForm.jsx
+++ src/components/reportForm.jsx
@@ -10,16 +10,38 @@ class ReactForm extends Form {
         errors: {}
     }
 
+
     schema = {
         dateInitial: Joi.date().format('YYYY/-MM-DD', 'DD-MM-YYY'),
         dateEnd: Joi.date().format('YYYY/-MM-DD', 'DD-MM-YYY')
 
     }
-    doSubmit = async () => {
 
+    doSubmit = async () => {
+        try {
+            const res = "" // here I should call my service  report
+            window.location = "/"
+        } catch (ex) {
+            if (ex.response && ex.response.status === 400) {
+                const errors = { ...this.state.errors };
+                errors.dateInitial = ex.response.data;
+                this.setState({ errors });
+            }
+        }
     }
 
+    render() {
+        return (
+            <div className="container">
+                <h1> Report Expenses</h1>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput("dateInitial", "DateInitial", "date")}\
+                    {this.renderInput("dateEnd", "DateEnd")}
+                </form>
 
-}
+            </div>
+        );
+    };
+};
 
 export default ReactForm;
