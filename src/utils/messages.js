f3a30604f369997a13770878e7525a8e79077d15
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-07-20 22:34:30 -0700

    update messages from report expenses

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,15 +2,16 @@ export function messages(expense) {
 
     const message = [
         'Spending too much money on',
-        'You should save money in this ',
-        'Spending too much money on this place '
-    ]
+        'Should save money in this categorie ',
+        'Saw that categorie you spending too much',
+        'See that you spend a lot there on '
 
+    ]
 
     const totalMessages = message.length;
 
     const messageSelect = Math.floor(Math.random() * totalMessages);
 
-    return message[messageSelect] + expense.toUpperCase() + " as always ";
+    return message[messageSelect] + " " + expense.toUpperCase() + " " + " as always ";
 
 }
